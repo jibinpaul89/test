110d76990b67d66b6249d88d4d41729ac485d7ad
Jibin Paul <jibin.paul@citrusinformatics.com>
2023-04-24 09:23:40 +0530

    Commit precommit1

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -2,10 +2,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    #path('', views.getsnippets, name='snippets'),
-    #path('api-auth/', include('rest_framework.urls'))
-
-
-
-    path('query/',           views.query_view),
+    # path('', views.getsnippets, name='snippets'),
+    # path('api-auth/', include('rest_framework.urls'))
+    path("query/", views.query_view),
 ]
